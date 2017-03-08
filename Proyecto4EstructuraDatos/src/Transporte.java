094db0d50fe1a31df2d8e78a90b41a161bedf086
Mario Raudales <MarioRaudales23@github.com>
2017-03-07 18:00:43 -0600

    Fifi Lo que tuu quiere2

--- Proyecto4EstructuraDatos/src/Transporte.java
+++ Proyecto4EstructuraDatos/src/Transporte.java
@@ -1,3 +1,6 @@
+
+import java.io.Serializable;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,7 +11,7 @@
  *
  * @author didiermurillo
  */
-public class Transporte {
+public class Transporte implements Serializable{
     public String Identificacion;
     public int Tiempo;
     public int Costo;
